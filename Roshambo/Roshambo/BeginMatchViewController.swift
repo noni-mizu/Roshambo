c0b2a32dd838264568c3dc9137d0ec424167daa3
noni <noni.mizu@gmail.com>
2016-06-08 12:30:44 -0700

    Initial commit

---
+++ Roshambo/Roshambo/BeginMatchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Winona Azure on 6/6/16.
+//  Copyright © 2016 Winona Azure. All rights reserved.
+//
+
+import UIKit
+
+class BeginMatchViewController: UIViewController {
+    
+    
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Roshambo/Roshambo/ResultsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsViewController.swift
+//  Roshambo
+//
+//  Created by Winona Azure on 6/6/16.
+//  Copyright © 2016 Winona Azure. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
